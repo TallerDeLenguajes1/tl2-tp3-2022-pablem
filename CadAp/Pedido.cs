cf20cace9d9b46b08fb5ac30ff63061781ada435
pablo <palmierijpablo@gmail.com>
2022-09-24 23:06:08 -0300

    relacion composición pedido-cliente

--- CadAp/Pedido.cs
+++ CadAp/Pedido.cs
@@ -8,12 +8,12 @@ public class Pedido
     Cliente cliente;
 
     //Constructor
-    public Pedido(string obs, Cliente cliente)
+    public Pedido(string nomb, string dire, string tel, string refe, string obs)
     {
         this.nro = ++pedidosRealizados;
         this.obs = obs;
         this.estado = EstadoPedido.Pendiente;
-        this.cliente = cliente;
+        this.cliente = new Cliente(nomb,dire,tel,refe);
     }
 
     //Getters & Setters
@@ -26,8 +26,5 @@ public class Pedido
     {
         Console.WriteLine("+ Pedido: {0} - Cliente: {1} - Estado: {2}", nro, cliente.Nombre, estado);
     }
-
-
-
 
 }
--- CadAp/Program.cs
+++ CadAp/Program.cs
@@ -4,7 +4,7 @@
     {
         List<Cadete> listaCadetes = cargarCadetes("datosCadetes.csv");
         List<Cadete> listaCadetes2 = cargarCadetes("datosCadetes2.csv");
-        List<Pedido> listaPedidos = cargarPedidos("datosClientesYPedidos.csv");
+        List<Pedido> listaPedidos = cargarPedidos("datosPedidos.csv");
         var cadeteria1 = new Cadeteria("Servimoto YB","4222222",listaCadetes);
         var cadeteria2 = new Cadeteria("Servimoto B Sur","4233333",listaCadetes2);
 
@@ -143,7 +143,6 @@
     private static List<Pedido> cargarPedidos(string ruta)
     {
         Pedido nuevoPedido;
-        Cliente nuevoCliente;
         var nuevaLista = new List<Pedido>();
         var listaCsv = HelperDeArchivo.LeerCsv(ruta);
         
@@ -151,8 +150,7 @@
             foreach (var pedido in listaCsv) {
                 if(pedido == null)
                     break;
-                nuevoCliente = new Cliente(pedido[0],pedido[1],pedido[2],pedido[3]);
-                nuevoPedido = new Pedido(pedido[4],nuevoCliente);
+                nuevoPedido = new Pedido(pedido[0],pedido[1],pedido[2],pedido[3],pedido[4]);
                 nuevaLista.Add(nuevoPedido);
             }
         } else {
